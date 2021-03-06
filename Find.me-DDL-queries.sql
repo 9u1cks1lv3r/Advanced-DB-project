62550dd4251e266c1ef35d8ccb43b1d3c0ee15e3
Yerkebulan07 <49221977+Yerkebulan07@users.noreply.github.com>
2021-03-06 22:26:57 +0600

    Update Find.me-DDL-queries.sql

--- Find.me-DDL-queries.sql
+++ Find.me-DDL-queries.sql
@@ -2,22 +2,22 @@ CREATE TABLE User (
 	user_Id INTEGER PRIMARY KEY,
 	login TEXT,
 	email TEXT,
-    password TEXT
+    	password TEXT
 );
 
 CREATE TABLE Blog (
 	blog_Id INTEGER PRIMARY KEY,
 	content TEXT,
-    date TEXT
+    	date TEXT
 );
 
 CREATE TABLE Profile (
 	profile_Id INTEGER PRIMARY KEY,
 	full_name TEXT,
 	age INTEGER,
-    about_me TEXT, 
-    contacts TEXT, 
-    rating INTEGER
+    	about_me TEXT, 
+    	contacts TEXT, 
+    	rating INTEGER
 );
 
 CREATE TABLE Review (
@@ -29,24 +29,24 @@ CREATE TABLE Review (
 CREATE TABLE Landlord (
 	landlord_Id INTEGER PRIMARY KEY,
 	full_name TEXT, 
-    contacts TEXT, 
-    rating INTEGER
+        contacts TEXT, 
+	rating INTEGER
 );
 
 CREATE TABLE Housing (
 	housing_Id INTEGER PRIMARY KEY,
 	title TEXT,
 	city TEXT,
-    description TEXT, 
-    rent_type TEXT, 
-    housing_type TEXT,
-    price INTEGER,
-    year INTEGER,
-    ceiling_height INTEGER,
-    material TEXT,
-    floorOfHousing INTEGER,
-    numberOfFloor INTEGER,
-    bathroom INTEGER,
-    internet BOOL,
-    balcony INTEGER
+        description TEXT, 
+    	rent_type TEXT, 
+    	housing_type TEXT,
+    	price INTEGER,
+    	year INTEGER,
+    	ceiling_height INTEGER,
+    	material TEXT,
+    	floorOfHousing INTEGER,
+    	numberOfFloor INTEGER,
+    	bathroom INTEGER,
+    	internet BOOL,
+    	balcony INTEGER
 );
