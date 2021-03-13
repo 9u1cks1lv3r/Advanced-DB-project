1703f82fd8e841e0a929b13dac31ade891288f6a
Yerkebulan07 <49221977+Yerkebulan07@users.noreply.github.com>
2021-03-13 19:08:16 +0600

    Update 14.sql

--- SQL_queries/14.sql
+++ SQL_queries/14.sql
@@ -1,3 +1,3 @@
 /*Which landlords rent apartments in Almaty?*/
-SELECT DISTINCT a.* FROM Lanlord a inner JOIN housing b ON a.landlord_Id = b.landlord_Id;
+SELECT DISTINCT a.* FROM Lanlord a INNER JOIN housing b ON a.landlord_Id = b.landlord_Id;
 
