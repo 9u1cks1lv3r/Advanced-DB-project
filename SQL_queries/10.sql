662c04a72093be5c00b255ac8171e7dfcd962094
Yerkebulan07 <49221977+Yerkebulan07@users.noreply.github.com>
2021-03-13 19:01:19 +0600

    Update 10.sql

--- SQL_queries/10.sql
+++ SQL_queries/10.sql
@@ -1,1 +1,3 @@
+/*Which landlord can give rent housing which floor of housing is lower than the average of the floor of housing in Almaty?  */
+SELECT l.landlord_id FROM Landlord l inner join housing h on a.landlordId = b_landlordId WHERE floorOfHousing < (SELECT avg(floorOfHousing) FROM housing WHERE city = “Almaty”)
 
