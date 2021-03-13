002ea96dc3922623bd8600c3ef5888040937fa59
Yerkebulan07 <49221977+Yerkebulan07@users.noreply.github.com>
2021-03-13 19:08:00 +0600

    Update 15.sql

--- SQL_queries/15.sql
+++ SQL_queries/15.sql
@@ -1,1 +1,3 @@
+/*Which profiles left the landlord report with ID equal to 4?*/
+SELECT a.profile_Id FROM Profile a INNER JOIN Review b ON a.profile_Id = b.profile_Id INNER JOIN Landlord c ON b.lanlord_Id = c.lanlord_Id WHERE c.lanlord_Id = 4;
 
