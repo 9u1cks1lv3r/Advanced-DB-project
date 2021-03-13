2c70f9d6445d23b811615ce0b88ad5fbaed526a8
Yerkebulan07 <49221977+Yerkebulan07@users.noreply.github.com>
2021-03-13 19:02:03 +0600

    Update 11.sql

--- SQL_queries/11.sql
+++ SQL_queries/11.sql
@@ -1,1 +1,3 @@
+/*What is the average price of apartments whose height is higher than the average height of all apartments?*/
+SELECT avg(price) FROM housing WHERE cellingHeight > (SELECT avg(price) FROM housing);
 
