0cb8d224fb993bda7c2f4d7944146d3be3e32304
Yerkebulan07 <49221977+Yerkebulan07@users.noreply.github.com>
2021-03-13 18:59:54 +0600

    Update 8.sql

--- SQL_queries/8.sql
+++ SQL_queries/8.sql
@@ -1,1 +1,3 @@
+/*How many housing will have balcony, bathroom will be “совмещенный” and type of rent will be by month?*/
+SELECT COUNT(*) FROM housing WHERE balcony = 1 AND bathroom = “совмещенный” AND rent_type = “Помесячно”;
 
