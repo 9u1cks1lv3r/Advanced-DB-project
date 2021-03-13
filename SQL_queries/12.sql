1b598d6ab6256223237cd5194168605704d82275
Yerkebulan07 <49221977+Yerkebulan07@users.noreply.github.com>
2021-03-13 19:03:41 +0600

    Update 12.sql

--- SQL_queries/12.sql
+++ SQL_queries/12.sql
@@ -1,1 +1,3 @@
+/*Which landlords have a power of attorney rating greater than the average rating of all landlords whose house is from Almaty?*/
+SELECT landlord_Id FROM Landlord WHERE rating > (SELECT avg(a.rating) FROM Landlord a inner JOIN housing b ON a.landlord_Id = b.landlord_Id WHERE b.city = ‘Алматы’);
 
