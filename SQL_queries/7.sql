feee0397146a00d42b03183eb41d9145f4918013
Yerkebulan07 <49221977+Yerkebulan07@users.noreply.github.com>
2021-03-28 18:53:43 +0600

    Update 7.sql

--- SQL_queries/7.sql
+++ SQL_queries/7.sql
@@ -1,2 +1,9 @@
-/*How will the user be added which login is 123, email is asd@gmail.com and password is 123456798 ?*/
-INSERT INTO User VALUES (1,123, asd@gmail.com, 123456798);
+--How update should works with attributes such as comics id, user id and series id?
+create or replace procedure update_user_list(
+        p_comics_id comics.comics_id%type,
+        p_user_id users.user_id%type,
+        p_series_id series.series_id%type)
+as
+begin
+    update user_list set last_chapter = p_comics_id where user_id = p_user_id and series_id = p_series_id;
+end;
