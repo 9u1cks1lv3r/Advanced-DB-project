da2d9dc3626cd39c18ac3160b5101ed699e8b03d
Yerkebulan07 <49221977+Yerkebulan07@users.noreply.github.com>
2021-03-28 19:02:36 +0600

    Update 11.sql

--- SQL_queries/11.sql
+++ SQL_queries/11.sql
@@ -1,3 +1,9 @@
-/*What is the average price of apartments whose height is higher than the average height of all apartments?*/
-SELECT avg(price) FROM housing WHERE cellingHeight > (SELECT avg(price) FROM housing);
-
+--How to check last_character is null value or not for User_List table?
+create or replace trigger user_list_trigger
+before insert on user_list
+for each row
+begin
+    if :new.last_chapter is null then
+        select min(comics_id) into :new.last_chapter from comics where series_id = :new.series_id;
+    end if;
+end;
