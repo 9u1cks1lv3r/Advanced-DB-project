40a96ccf3855db890954a1e7f36ddc3c7e675b6c
Yerkebulan07 <49221977+Yerkebulan07@users.noreply.github.com>
2021-03-28 19:11:45 +0600

    Update 14.sql

--- SQL_queries/14.sql
+++ SQL_queries/14.sql
@@ -1,4 +1,6 @@
 --How to check series' id is null value or not for Series table?
+CREATE SEQUENCE series_seq START WITH 1;
+
 create or replace trigger series_id_inc 
     before insert on series
     for each row
