e2a207a061346a88851f29d4ed1d2715c61d6b73
Yerkebulan07 <49221977+Yerkebulan07@users.noreply.github.com>
2021-03-28 18:32:18 +0600

    Update 3.sql

--- SQL_queries/3.sql
+++ SQL_queries/3.sql
@@ -1,3 +1,9 @@
-/*How many housings with internet ADSL in Nur-Sultan?*/
-SELECT * FROM housing WHERE internet = “ADSL” AND city = “Нур-Султан”
-
+--What will we realize to add user_id and series_id to list?
+create or replace procedure add_to_list(
+        p_user_id in users.user_id%type,
+        p_series_id in series.series_id%type)
+as
+begin
+    INSERT INTO USER_LIST (user_id, series_id) VALUES(p_user_Id, p_series_id);
+    commit;
+end;
