c06d0cca0c9fb2d78a7d10698582f6782beee50a
Yerkebulan07 <49221977+Yerkebulan07@users.noreply.github.com>
2021-03-28 18:27:42 +0600

    Update 2.sql

--- SQL_queries/2.sql
+++ SQL_queries/2.sql
@@ -1,3 +1,13 @@
-/*What housing has a bigger price where  rent type is by hours?*/
-SELECT * FROM housing WHERE price = (SELECT max(price) FROM housings) AND rent_type = “По часам”;
-
+--How can get information about genre of comics?
+create or replace function get_genre_name(
+    p_genre_id number) 
+    return varchar2 is
+        cursor get_name is
+            select g_name from genres where genres_id = p_genre_id;
+        v_genre  genres.genres_id%type;
+begin
+    open get_name;
+    fetch get_name into v_genre;
+    close get_name;
+    return v_genre;
+end;
