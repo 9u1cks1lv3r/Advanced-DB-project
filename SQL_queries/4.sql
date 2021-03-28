9ad1a948784b3254ed73bf7c7c11cf4f0f67492d
Yerkebulan07 <49221977+Yerkebulan07@users.noreply.github.com>
2021-03-28 18:33:15 +0600

    Update 4.sql

--- SQL_queries/4.sql
+++ SQL_queries/4.sql
@@ -1,3 +1,10 @@
-/*What housing is the oldest ?*/
-SELECT * FROM housing WHERE year = (SELECT max(year) FROM housings)
+--Which procedure or function helps us with removing certain object in list?
+create or replace procedure delete_from_list(
+        p_user_id in users.user_id%type,
+        p_series_id in series.series_id%type)
+as
+begin
+    delete from user_list where user_id = p_user_id and series_id = p_series_id;
+    commit;
+end;
 
