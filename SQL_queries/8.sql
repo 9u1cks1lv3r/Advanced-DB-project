76da478f6fc93f02cd6dd83e6e2499b4225ea759
Yerkebulan07 <49221977+Yerkebulan07@users.noreply.github.com>
2021-03-28 18:55:22 +0600

    Update 8.sql

--- SQL_queries/8.sql
+++ SQL_queries/8.sql
@@ -1,3 +1,22 @@
-/*How many housing will have balcony, bathroom will be “совмещенный” and type of rent will be by month?*/
-SELECT COUNT(*) FROM housing WHERE balcony = 1 AND bathroom = “совмещенный” AND rent_type = “Помесячно”;
-
+--Which trigger should we use to check existing user's username or email?
+CREATE OR REPLACE TRIGGER user_check
+BEFORE INSERT ON users
+FOR EACH ROW
+DECLARE
+    TYPE Checker IS TABLE OF NUMBER;
+    chkr Checker := Checker(0, 0);
+BEGIN
+    SELECT COUNT(*) INTO chkr(2) FROM users WHERE username = :NEW.username;
+    SELECT COUNT(*) INTO chkr(1) FROM users WHERE email = :NEW.email;
+    IF (chkr(2) >= 1) THEN
+        IF chkr(1) >= 1 THEN
+            raise_application_error(-20001,'User with this username and email is already created!');
+        ELSE
+            raise_application_error(-20001,'User with this username is already created!');
+        END IF;
+    ELSE
+        IF chkr(1) >= 1 THEN
+            raise_application_error(-20001,'User with this email is already created!');
+        END IF;      
+    END IF;
+END;
