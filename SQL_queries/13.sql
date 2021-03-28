96ced3bcaf72a817cbd24e0734301c60c8b69243
Yerkebulan07 <49221977+Yerkebulan07@users.noreply.github.com>
2021-03-28 19:04:02 +0600

    Update 13.sql

--- SQL_queries/13.sql
+++ SQL_queries/13.sql
@@ -1,3 +1,9 @@
-/*Which ads have "недорого" in their titles?*/
-SELECT * FROM housing WHERE title LIKE ‘%недорого%’;
-
+--How to check user's id is null value or not for Users table?
+create or replace trigger users_id_inc 
+    before insert on users
+    for each row
+begin
+    if :new.user_id is null then
+        select users_seq.nextval into :new.user_ID from dual;
+    end if;
+end;
