365441de55614a2fbc884bd24fd177d381e08158
Yerkebulan07 <49221977+Yerkebulan07@users.noreply.github.com>
2021-03-28 18:48:09 +0600

    Update 6.sql

--- SQL_queries/6.sql
+++ SQL_queries/6.sql
@@ -1,3 +1,18 @@
-/*What landlord’s full name which rating is 10? */
-SELECT Full_name FROM Landlord WHERE rating = 10;
-
+--What function should we do to get user id using user's email and user's password?
+create or replace function get_user_id(
+        p_email users.email%type,
+        p_password users.password%type)
+    return number
+as 
+    cursor c_user is
+        select * from users where email = p_email and password = p_password;
+    v_user users%rowtype;
+begin
+    open c_user;
+    fetch c_user into v_user;
+    if c_user%notfound then
+        return 0;
+    end if;
+    close c_user;
+    return v_user.user_id;
+end;
