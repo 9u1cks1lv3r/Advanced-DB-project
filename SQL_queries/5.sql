7ba682df9f53cfc5cd3a4465ab679fa4c2187f11
Yerkebulan07 <49221977+Yerkebulan07@users.noreply.github.com>
2021-03-28 18:41:38 +0600

    Update 5.sql

--- SQL_queries/5.sql
+++ SQL_queries/5.sql
@@ -1,3 +1,10 @@
-/*How many flats are rents where material is Kirpich?*/
-SELECT * FROM housing WHERE housing_type = “flat”;
-
+--How Users can registrate to the site?
+create or replace procedure insert_user(
+        p_username users.username%type,
+        p_password users.password%type,
+        p_email users.email%type)
+as
+begin
+    insert into users(username, password, email) values(p_username, p_password, p_email);
+    commit;
+end;
